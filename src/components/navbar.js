c2cdc1d1ed22d4302d717f213f9fafe7621c94be
Michael McKay <mamckay89@gmail.com>
2019-04-08 16:47:57 -0600

    working on the main design

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,8 +10,8 @@ class Navbar extends React.Component {
     this.setup();
   }
   setup() {
-    if (localStorage.getItem('authToken') !== 'null') {
-      console.log(localStorage.getItem('authToken'))
+    if (localStorage.getItem('authToken') !== 'null' || localStorage.getItem('authToken') !== null) {
+      console.log(localStorage.getItem('authToken'));
       this.props.dispatch(relog(localStorage.getItem('authToken')));
     }
   }
